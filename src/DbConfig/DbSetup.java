6a2c749bbfe3d9d8921ce97d1212ca03f0224f7c
Sawaid-hub <Sawaid-hub@users.noreply.github.com>
2026-01-13 19:38:10 +0500

    Fix DbSetup configuration

--- src/DbConfig/DbSetup.java
+++ src/DbConfig/DbSetup.java
@@ -2,6 +2,9 @@ package DbConfig;
 
 import java.util.Properties;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DbSetup {
 
